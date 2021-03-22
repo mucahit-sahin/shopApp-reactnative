4ef1cff1e984d772baed1b3bdee0a4da76014504
mucahit-sahin <mucahitsahin786@gmail.com>
2021-03-22 13:38:35 +0300

    Create TabNavigation

---
+++ src/Navigations/TabNavigation.js
@@ -0,0 +1,44 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from 'react';
+import Home from '../Screens/Home';
+import Orders from '../Screens/Orders';
+import Profile from '../Screens/Profile';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const Tab = createBottomTabNavigator();
+
+const TabNavigation = () => {
+  return (
+    <Tab.Navigator initialRouteName="Home">
+      <Tab.Screen
+        name="Orders"
+        component={Orders}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="th-large" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({color, size}) => (
+            <Icon name="user" size={size} color={color} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigation;
