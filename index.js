45b1bdbb89f47f2db1d4bb1cc843f0f6de0c0c0d
mucahit-sahin <mucahitsahin786@gmail.com>
2021-04-30 13:58:27 +0300

    Created store

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/App';
 import {name as appName} from './app.json';
+import ProviderApp from './ProviderApp';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => ProviderApp);
--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
+import {useDispatch} from 'react-redux';
 
 import SignNavigation from './Navigations/SignNavigation';
+import TabNavigation from './Navigations/TabNavigation';
+import CreateShop from './Screens/CreateShop';
+import {setUserAction} from './Store/Actions/authActions';
 
 const App = () => {
-  return <SignNavigation />;
+  const [initializing, setInitializing] = React.useState(true);
+  const [user, setUser] = React.useState();
+  const [userDocument, setUserDocument] = React.useState();
+  const dispatch = useDispatch();
+  // Handle user state changes
+  function onAuthStateChanged(user) {
+    setUser(user);
+    dispatch(setUserAction(user));
+    if (initializing) setInitializing(false);
+  }
+
+  React.useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
+  }, [user]);
+
+  if (initializing) return null;
+
+  if (!user) {
+    return <SignNavigation />;
+  }
+
+  firestore()
+    .collection('Shops')
+    .doc(user?.email)
+    .onSnapshot(documentSnapshot => {
+      setUserDocument(documentSnapshot.data());
+    });
+  if (userDocument) {
+    return <TabNavigation user={user} />;
+  }
+  return <CreateShop user={user} />;
 };
 
 export default App;
